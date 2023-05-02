3ea48cce76e22de91721cdd6cd5a2db3d0435376
Shepon Biswas <sheponbiswas01@gmail.com>
2023-05-03 03:57:23 +0600

    Chefs Json Data added & send chefs data response

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 const port = 5000;
 
+const chefs = require('./data/chefs.json');
+
+app.use(cors())
+
 app.get('/', (req, res)=>{
     res.send("The Chinese Chef Recipe is Running..")
+})
+// Send Data Response for chefs
+app.get('/chefs', (req, res)=>{
+    res.send(chefs);
 })
 
 app.listen(port, ()=>{
