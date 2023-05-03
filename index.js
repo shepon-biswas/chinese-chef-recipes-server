2bc58b9e9ef6feef5e51f85bcc5ed18de87b115b
Shepon Biswas <sheponbiswas01@gmail.com>
2023-05-04 03:59:52 +0600

    Created recipes JSON data and loaded them

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 5000;
 
 const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipes.json');
 
 app.use(cors())
 
@@ -13,6 +14,16 @@ app.get('/', (req, res)=>{
 // Send Data Response for chefs
 app.get('/chefs', (req, res)=>{
     res.send(chefs);
+})
+
+// Send Data Response for Recipes
+app.get('/recipes', (req, res) =>{
+    res.send(recipes);
+})
+app.get('/recipes/:id', (req, res) =>{
+    const chefId = req.params.id;
+    const chefRecipes = recipes.filter(recipe => recipe.chef_id == chefId);
+    res.send(chefRecipes);
 })
 
 app.listen(port, ()=>{
